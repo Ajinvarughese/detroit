b6a278b98911fd5e6dc0ae41e157da41577ef81a
Ajin Varughese <ajinvarughese91@gmail.com>
2024-12-23 20:08:07 +0530

    Error fix

--- src/main/java/com/Detriot/detroit/Controller.java
+++ src/main/java/com/Detriot/detroit/Controller.java
@@ -1,9 +1,11 @@
 package com.Detriot.detroit;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("api/")
+@RestController
+@RequestMapping("api/")
 public class Controller {
     @GetMapping("/show")
     public String helloWorld() {
