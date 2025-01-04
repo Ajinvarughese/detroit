2d36c675b90882c185bc472f3932599f08c5e48d
Ajin Varughese <ajinvarughese91@gmail.com>
2025-01-04 11:26:01 +0530

    update

---
+++ src/main/java/com/Detriot/detroit/Controller/QuestionnaireController.java
@@ -0,0 +1,10 @@
+package com.Detriot.detroit.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/questionnaire")
+public class QuestionnaireController {
+
+}
