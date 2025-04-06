50e47d85627f1a1b938ff236cf71632f502ff1d5
Muhammed Aslam <aslamnazi1234@gmail.com>
2025-04-06 17:46:15 +0530

    Questionnair completed

--- src/main/java/com/Detriot/detroit/controller/QuestionnaireController.java
+++ src/main/java/com/Detriot/detroit/controller/QuestionnaireController.java
@@ -1,10 +1,61 @@
 package com.Detriot.detroit.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Detriot.detroit.entity.Questionnaire;
+import com.Detriot.detroit.service.QuestionnaireService;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/questionnaire")
+@AllArgsConstructor
 public class QuestionnaireController {
+    private final QuestionnaireService questionnaireService;
 
+    //Get all questions
+    @GetMapping
+    public ResponseEntity<List<Questionnaire>> getAllQuestionnaire(){
+       return ResponseEntity.ok(questionnaireService.getAllQuestionnaire());
+    }
+
+    //Get questionnaire by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Questionnaire> getQuestionnaireById(@PathVariable Long id){
+        return ResponseEntity.ok(questionnaireService.getQuestionnaireById(id));
+    }
+
+    // Update an existing questionnaire
+    @PutMapping("/{id}")
+    public ResponseEntity<Questionnaire> updateQuestionnaire(@PathVariable Long id ,@RequestBody Questionnaire questionnaire) {
+        try {
+            return ResponseEntity.ok(questionnaireService.updateQuestionnaire(id ,questionnaire));
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // deleting an existing questionnaire
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Questionnaire> deleteQuestionnaire(@PathVariable Long id){
+        try {
+            questionnaireService.deleteQuestionnaire(id);
+            return ResponseEntity.noContent().build();
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+ /*
+        TODO:
+              * Get all question
+              * Get questionnaire by ID
+              * Update an existing questionnaire
+              * Delete a questionnaire
+
+    */
 }
+
--- src/main/java/com/Detriot/detroit/service/QuestionnaireService.java
+++ src/main/java/com/Detriot/detroit/service/QuestionnaireService.java
@@ -1,4 +1,60 @@
 package com.Detriot.detroit.service;
 
+import com.Detriot.detroit.entity.Questionnaire;
+import com.Detriot.detroit.repository.QuestionnaireRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+
 public class QuestionnaireService {
+
+    private final QuestionnaireRepository questionnaireRepository;
+
+    //Get all questions
+    public List<Questionnaire> getAllQuestionnaire(){ return questionnaireRepository.findAll(); }
+
+
+    //Get Specific questionnaire by id
+    public Questionnaire getQuestionnaireById (Long id){
+        return questionnaireRepository.findById(id)
+                .orElseThrow(()-> new  EntityNotFoundException("Questionnaire not found with id: "+id));
+    }
+
+    //Create a new Questionnaire
+    public Questionnaire addQuestionnaire(Questionnaire questionnaire){
+        return questionnaireRepository.save(questionnaire);
+    }
+
+    //update an existing questionnaire
+    public Questionnaire updateQuestionnaire(Long id,Questionnaire updatedQuestionnaire){
+        return questionnaireRepository.findById(id).map( questionnaire -> {
+            questionnaire.setTitle(updatedQuestionnaire.getTitle());
+            questionnaire.setDescription(updatedQuestionnaire.getDescription());
+            return questionnaireRepository.save(questionnaire);
+        }).orElseThrow(() -> new IllegalArgumentException("Questionnaire not found with id:"+id));
+    }
+    //delete  a questionnaire by id
+
+    public void deleteQuestionnaire(Long id){
+        if (!questionnaireRepository.existsById(id)) {
+            throw new IllegalArgumentException("Questionnaire not found with id:"+id);
+        }
+        questionnaireRepository.deleteById(id);
+    }
 }
+
+ /*
+        TODO:
+              * Get All Questionnaires !
+              * get Specific questionnaire by id
+              * Create a new Questionnaire
+              * update an existing questionnaire
+              * delete  a questionnaire by id
+
+    */
